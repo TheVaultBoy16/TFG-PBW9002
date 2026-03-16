c979cb57f347787ee651e8c3f5c30d8cc3595dac
TheVaultBoy16 <gonaguiman@gmail.com>
2026-03-16 10:34:30 +0100

    Añadido scroll a VmScreen

--- app/src/main/java/com/example/myapplication/ui/vm/VmScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/vm/VmScreen.kt
@@ -11,7 +11,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenuItem
@@ -57,10 +59,13 @@ fun VmScreen(
     val displayState = if (isRunning) "Ejecutándose" else "Apagada"
     val stateColor = if (isRunning) Color(0xFF4CAF50) else Color.Gray
     val imageRes = if (isRunning) R.drawable.ejecutandose else R.drawable.apagada
+    
+    val scrollState = rememberScrollState()
 
     Column(
         modifier = modifier
             .fillMaxSize()
+            .verticalScroll(scrollState)
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
